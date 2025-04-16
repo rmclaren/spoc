04bf3a15916dfd64dfd88b14f3a4e7c2eb82b710
Nicholas Esposito <esposito@hercules-login-3.hpc.msstate.edu>
2025-04-16 15:30:40 -0400

    add aircraft

---
+++ dump/mapping/prepbufr_acft_profiles.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+import os
+import sys
+import argparse
+import bufr
+import copy
+import numpy as np
+import numpy.ma as ma
+import math
+import calendar
+import time
+from datetime import datetime
+
+import bufr
+from bufr.obs_builder import add_main_functions
+from prepbufr_obs_builder import PrepbufrObsBuilder, map_path
+
+
+MAPPING_PATH = map_path('prepbufr_acft_profiles.yaml')
+
+class AcftProfilesPrepbufrObsBuilder(PrepbufrObsBuilder):
+    def __init__(self):
+        super().__init__(MAPPING_PATH, log_name=os.path.basename(__file__))
+
+    def _make_description(self):
+        description = super()._make_description()
+
+        description.add_variables([ 
+            {
+                'name': 'MetaData/sequenceNumber',
+                'source': 'sequenceNumber',
+                'units': '1',
+                'longName': 'Sequence Number (Obs Subtype)',
+            },
+        ])
+
+        return description
+
+
+    def make_obs(self, comm, input_path):
+        """
+        Create the ioda acft_profiles prepbufr observations:
+        - reads values
+        - adds sequenceNum
+    
+        Parameters
+        ----------
+        comm: object
+                The communicator object (e.g., MPI)
+        input_path: str
+                The input bufr file
+        mapping_path: str
+                The input bufr2ioda mapping file
+        cycle_time: str
+                The cycle in YYYYMMDDHH format
+        """
+    
+        # Get container from mapping file first
+        self.log.info(f'Get container from bufr')
+        container = super().make_obs(comm, input_path)
+
+        self.log.debug(f'container list (original): {container.list()}')
+        #self.log.debug(f'Change longitude range from [0,360] to [-180,180]')
+        lon = container.get('longitude')
+        lon_paths = container.get_paths('longitude')
+    
+        self.log.debug(f'Do DateTime calculation')
+        dhr = container.get('obsTimeMinusCycleTime')
+        dhr_paths = container.get_paths('obsTimeMinusCycleTime')
+        dhr2 = np.array(dhr)
+        self._replace_timestamp(container, self._get_reference_time(input_path))
+    
+        self.log.debug(f'Make an array of 0s for MetaData/sequenceNumber')
+        sequenceNum = self._compute_sequence_number(lon) 
+        self.log.debug(f'sequenceNum min/max =  {sequenceNum.min()} {sequenceNum.max()}')
+    
+        self.log.debug(f'Compute Obstypes')
+        t_ot = container.get('airTemperatureObservationType')
+        #tv_ot = container.get('virtualTemperatureObservationType')
+        q_ot = container.get('specificHumidityObservationType')
+        uv_ot = container.get('windObservationType')
+        ot_paths = container.get_paths('airTemperatureObservationType')
+    
+        airTemperature = container.get('airTemperatureObsValue')
+        #virtualTemperature = container.get('virtualTemperatureObsValue')
+        specificHumidity = container.get('specificHumidityObsValue')
+        wind = container.get('windNorthwardObsValue')
+    
+        ot_airTemperature = self._compute_typ_other(t_ot, airTemperature)
+        #ot_virtualTemperature = self._compute_typ_other(tv_ot, virtualTemperature)
+        ot_specificHumidity = self._compute_typ_other(q_ot, specificHumidity)
+        ot_wind = self._compute_typ_uv(uv_ot, wind)
+    
+        self.log.debug(f'Change IALR to 0.0 if masked for bias correction.')
+        ialr = container.get('instantaneousAltitudeRate')
+        ialr_paths = container.get_paths('instantaneousAltitudeRate')
+        ialr2 = ma.array(ialr)
+    
+        ialr_bc = self._compute_ialr_if_masked(uv_ot, ialr2)
+    
+        self.log.debug(f'Update variables in container')
+        container.replace('instantaneousAltitudeRate', ialr_bc)#, ot_paths)
+        container.replace('airTemperatureObservationType', ot_airTemperature)
+        #container.replace('virtualTemperatureObservationType', ot_virtualTemperature)
+        container.replace('specificHumidityObservationType', ot_specificHumidity)
+        container.replace('windObservationType', ot_wind)
+    
+        self.log.debug(f'Add variables to container')
+        container.add('sequenceNumber', sequenceNum, lon_paths)
+    
+        # Check
+        self.log.debug(f'container list (updated): {container.list()}')
+    
+        return container
+    
+    
+    def _compute_typ_other(self, typ, var):
+        """
+        Compute datatype if the variable is not wind.
+        Parameters:
+            typ: datatype
+            var: obsValue variable
+        Returns:
+            Masked array of the new datatype
+        """
+    
+        typ_var = copy.deepcopy(typ)
+        typ_var[(typ_var > 300) & (typ_var < 400)] -= 200
+        typ_var[(typ_var > 400) & (typ_var < 500)] -= 300
+        typ_var[(typ_var > 500) & (typ_var < 600)] -= 400
+    
+        for i in range(len(typ_var)):
+            if ma.is_masked(var[i]):
+                typ_var[i] = typ_var.fill_value
+    
+        return typ_var
+    
+    
+    def _compute_typ_uv(self, typ, var):
+        """
+        Compute datatype if the variable is wind.
+        Parameters:
+            typ: datatype
+            var: obsValue variable
+        Returns:
+            Masked array of the new datatype
+        """
+    
+        typ_var = copy.deepcopy(typ)
+        typ_var[(typ_var > 300) & (typ_var < 400)] -= 100
+        typ_var[(typ_var > 400) & (typ_var < 500)] -= 200
+        typ_var[(typ_var > 500) & (typ_var < 600)] -= 300
+    
+        for i in range(len(typ_var)):
+            if ma.is_masked(var[i]):
+                typ_var[i] = typ_var.fill_value
+    
+        return typ_var
+    
+    
+    def _compute_ialr_if_masked(self, typ, ialr):
+        """
+        Compute instantaneousAltitudeRate (IALR) if it is masked.
+        Parameters:
+            typ: datatype
+            ialr: instantaneousAltitudeRate
+        Returns:
+            Masked array of the updated instantaneousAltitudeRate
+        """
+    
+        ialr_bc = copy.deepcopy(ialr)
+        for i in range(len(ialr_bc)):
+            if ma.is_masked(ialr_bc[i]) and (typ[i] >= 330) and (typ[i] < 340):
+                ialr_bc[i] = float(0)
+    
+        return ialr_bc
+
+
+    def _compute_sequence_number(self, lon): 
+        """
+        Compute sequenceNumber
+
+        Parameters:
+            lon: longitude 
+
+        Returns:
+            Masked array of sequenceNumber values. 
+            In this case, array is all 0's.
+        """
+
+        sequenceNumber = np.zeros(lon.shape, dtype=np.int32)
+
+        return sequenceNumber
+
+
+add_main_functions(AcftProfilesPrepbufrObsBuilder)
