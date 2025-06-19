e678ef0f07b1f5eef336053adfbc3ca016272b7d
emilyhcliu <36091766+emilyhcliu@users.noreply.github.com>
2025-06-19 13:46:09 -0400

    Add ObsBuilder for Ozone (#47)
    **This PR adds the mapping file and Python script for the following
    ozone data:**
    - omi aura (total ozone)
    - ompstc (total ozone)
    - ompsnp (profile ozone)
    
    **1.** BufrOzoneObsBuilder - base class for all ozone obs builder
     - BufrOzone**Omi**ObsBuilder
     - BufrOzone**Ompstc**ObsBuilder
     - BufrOzone**Ompsnp**ObsBuilder
    
    **2.** IODA validator checked!
    
    [ObsSpace.yaml](https://github.com/JCSDA-internal/ioda/blob/feature/ObsSpace_Validator/share/ioda/yaml/validation/ObsSpace.yaml)
    (IODA feature/ObsSpace_Validator) is updated to include elements for
    ozone-related variables
    
    **3.** Coding Norms checked and passed!
    
    **4.** Sphinx style comments added 
    
    **Notes: Issue to be fixed in the future** (see [bufr-query Issue
    #100](https://github.com/NOAA-EMC/bufr-query/issues/100))
    The new dimension name is not replaced with the specified name "Vertice"
    (stays as the default name: dim_2)
    
    ```
    netcdf gdas.t00z.ozone.ompsnp_n20.tm00 {
    dimensions:
            Location = 32172 ;
            dim_2 = 2 ;
    variables:
            int Location(Location) ;
                    Location:_FillValue = 2147483647 ;
            int dim_2(dim_2) ;
                    dim_2:_FillValue = 2147483647 ;
    ```

---
+++ dump/mapping/bufr_ozone_obs_builder.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+import os
+import numpy as np
+import numpy.ma as ma
+
+import bufr
+from bufr.obs_builder import ObsBuilder, add_dummy_variable
+
+
+class BufrOzoneObsBuilder(ObsBuilder):
+    """
+    Observations builder for BUFR Ozone data.
+
+    Inherits from ObsBuilder and provides specific logic for ozone observation processing.
+    """
+
+    def __init__(self, mapping_path, log_name=os.path.basename(__file__)):
+        super().__init__(mapping_path, log_name=log_name)
+
+    def make_obs(self, comm, input_path):
+        # Get container from mapping file first
+        self.log.info('Get container from bufr')
+        container = super().make_obs(comm, input_path)
+
+        self.log.debug(f'container list (original): {container.list()}')
+        self.log.debug(f'all_sub_categories =  {container.all_sub_categories()}')
+        self.log.debug(f'category map =  {container.get_category_map()}')
+
+        # Add new/derived data into container
+        for cat in container.all_sub_categories():
+
+            self.log.debug(f'category = {cat}')
+
+            satId = container.get('satelliteId', cat)
+            if not np.any(satId):
+                self.log.warning(f'category {cat[0]} does not exist in input file')
+
+            self._add_pressures(container, cat)
+
+        # Check
+        self.log.debug(f'container list (updated): {container.list()}')
+        self.log.debug(f'all_sub_categories {container.all_sub_categories()}')
+
+        return container
+
+    def _make_description(self):
+        description = super()._make_description()
+        self._add_new_variable_descriptions(description)
+
+        return description
+
+    def _add_new_variable_descriptions(self, description):
+        description.add_variables([
+            {
+                'name': 'MetaData/pressure',
+                'source': 'pressure',
+                'units': 'Pa',
+                'longName': 'Pressure',
+            }])
+
+    def _add_pressures(self, container, cat):
+        """
+        Add pressure metadata to the container for a given category.
+
+        :param container: Observation container.
+        :type container: object
+        :param cat: Category identifier (tuple or similar).
+        :type cat: object
+        """
+        # Add new MetaData variables: MetaData/pressure
+        satId = container.get('satelliteId', cat)
+        if not satId.size:
+            self.log.warning(f'category {cat[0]} does not exist in input file')
+            add_dummy_variable(container, 'pressure', cat, 'latitude')
+            return
+
+        latitude = container.get('latitude', cat)
+        paths = container.get_paths('latitude', cat)
+
+        pressure = np.full_like(latitude, 0)
+        container.add('pressure', pressure, paths, cat)
---
+++ dump/mapping/bufr_ozone_ompstc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import os
+import numpy as np
+import numpy.ma as ma
+
+import bufr
+from bufr.obs_builder import ObsBuilder, add_main_functions, map_path
+from bufr_ozone_obs_builder import BufrOzoneObsBuilder
+
+MAPPING_PATH = map_path('bufr_ozone_ompstc.yaml')
+
+
+class BufrOzoneOmpstcObsBuilder(BufrOzoneObsBuilder):
+    """
+    Class for building observations from ompst8 BUFR data.
+
+    This class extends `ObsBuilder` to include specific logic for processing
+    Level-2 retrived total ozone data from OMPS nadir mapper
+
+    :param mapping_path: Path to the mapping file.
+    :type mapping_path: str
+    """
+
+    def __init__(self):
+        super().__init__(MAPPING_PATH, log_name=os.path.basename(__file__))
+
+
+# Add main functions create_obs_file or create_obs_group
+add_main_functions(BufrOzoneOmpstcObsBuilder)
