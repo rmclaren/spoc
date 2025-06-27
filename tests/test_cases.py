7f085f27010bc3c5e523cc1384ffb3e3014079e1
Ron McLaren <ronald.mclaren@noaa.gov>
2025-06-27 15:17:33 -0400

    updated test_cases with correct outputs

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -16,8 +16,8 @@ TEST_CASES = [
             'input': 'testdata/2025040100-gdas.t00z.satwnd.tm00.bufr_d',
             'output': 'testrun/bufr_satwnd_amv_ahi_{splits/satId}.nc'
         },
-        'cmp': 'testoutput/bufr_satwnd_amv_ahi_goes-16.nc',
-        'result': 'testrun/bufr_satwnd_amv_ahi_goes-16.nc'
+        'cmp': 'testoutput/bufr_satwnd_amv_ahi_h9.nc',
+        'result': 'testrun/bufr_satwnd_amv_ahi_h9.nc'
     },
     {
         'map': 'bufr_satwnd_amv_avhrr',
@@ -25,8 +25,8 @@ TEST_CASES = [
             'input': 'testdata/2025040100-gdas.t00z.satwnd.tm00.bufr_d',
             'output': 'testrun/bufr_satwnd_amv_avhrr_{splits/satId}.nc'
         },
-        'cmp': 'testoutput/bufr_satwnd_amv_avhrr_goes-16.nc',
-        'result': 'testrun/bufr_satwnd_amv_avhrr_goes-16.nc'
+        'cmp': 'testoutput/bufr_satwnd_amv_avhrr_n19.nc',
+        'result': 'testrun/bufr_satwnd_amv_avhrr_n19.nc'
     },
     {
         'map': 'bufr_satwnd_amv_leogeo',
@@ -34,8 +34,8 @@ TEST_CASES = [
             'input': 'testdata/2025040100-gdas.t00z.satwnd.tm00.bufr_d',
             'output': 'testrun/bufr_satwnd_amv_leogeo_{splits/satId}.nc'
         },
-        'cmp': 'testoutput/bufr_satwnd_amv_leogeo_goes-16.nc',
-        'result': 'testrun/bufr_satwnd_amv_leogeo_goes-16.nc'
+        'cmp': 'testoutput/bufr_satwnd_amv_leogeo_multi.nc',
+        'result': 'testrun/bufr_satwnd_amv_leogeo_multi.nc'
     },
     {
         'map': 'bufr_satwnd_amv_modis',
@@ -43,8 +43,8 @@ TEST_CASES = [
             'input': 'testdata/2025040100-gdas.t00z.satwnd.tm00.bufr_d',
             'output': 'testrun/bufr_satwnd_amv_modis_{splits/satId}.nc'
         },
-        'cmp': 'testoutput/bufr_satwnd_amv_modis_goes-16.nc',
-        'result': 'testrun/bufr_satwnd_amv_modis_goes-16.nc'
+        'cmp': 'testoutput/bufr_satwnd_amv_modis_terra.nc',
+        'result': 'testrun/bufr_satwnd_amv_modis_terra.nc'
     },
     {
         'map': 'bufr_satwnd_amv_seviri',
@@ -52,8 +52,8 @@ TEST_CASES = [
             'input': 'testdata/2025040100-gdas.t00z.satwnd.tm00.bufr_d',
             'output': 'testrun/bufr_satwnd_amv_seviri_{splits/satId}.nc'
         },
-        'cmp': 'testoutput/bufr_satwnd_amv_seviri_goes-16.nc',
-        'result': 'testrun/bufr_satwnd_amv_seviri_goes-16.nc'
+        'cmp': 'testoutput/bufr_satwnd_amv_seviri_m10.nc',
+        'result': 'testrun/bufr_satwnd_amv_seviri_m10.nc'
     },
 
 ]
